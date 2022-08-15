d0dfa06b302702e58623a980cebbe818ddfc287e
TheMegistone4Ever <zeusmobilenick@gmail.com>
2022-08-15 15:47:26 +0300

    alpha

--- Coursera4CPPHexProgram/Coursera4CPPHexProgram.cpp
+++ Coursera4CPPHexProgram/Coursera4CPPHexProgram.cpp
@@ -5,8 +5,20 @@
 #include "HexBoardASCII.h"
 
 int main(int argc, char const* argv[]) {
-    srand(static_cast<unsigned int>(time(nullptr)));
+    //srand(static_cast<unsigned int>(time(nullptr)));
 
+    short size; cout << "Input size of a game: "; cin >> size;
+
+    HexBoard board(size);
+    board.print();
+
+    for (short k = 0; k < size * size; k++) {
+        cout << "Player #" << k % 2 + 1 << " make move! (Input \'i\' and \'j\')" << endl;
+        short i;  cout << "i: "; cin >> i;
+        short j;  cout << "j: "; cin >> j;
+        if (!board.makeMoveIn(i, j, k % 2 + 1)) k--;
+        board.print();
+    }
 
 
     return EXIT_SUCCESS;
--- Coursera4CPPHexProgram/HexBoardASCII.h
+++ Coursera4CPPHexProgram/HexBoardASCII.h
@@ -1,2 +1,59 @@
 #pragma once
 #include <iostream>
+#include <vector>
+using namespace std;
+
+inline void printStrs(short n, const char* s) {
+	for (int i = 0; i < n; i++) cout << s;
+}
+
+class HexBoard {
+	short size; // size of the board
+	short** board; // field of action, 0 - empty, 1 - player ¹1 walked, 2 - player ¹2 walked
+public:
+	HexBoard(short size = 7) : size(size), board(new short*[size]) {
+		for (int i = 0; i < size; i++) {
+			board[i] = new short[size];
+			for (int j = 0; j < size; j++) board[i][j] = 0;
+		}
+	}
+	~HexBoard() {
+		for (int i = 0; i < size; i++) delete[] board[i];
+		delete[] board;
+	}
+
+	// Checking if someone has won
+	short hasWon() {
+
+	}
+
+	// Takes a move to a given position
+	bool makeMoveIn(short i, short j, short player) {
+		if (i > size - 1 || j > size - 1) {
+			cerr << "Out of bounds move!" << endl;
+			return false;
+		}
+		if (board[i][j]) {
+			cerr << "There is already another player's move at position i:" << i << " j:" << j << endl;
+			return false;
+		}
+		board[i][j] = player;
+		cout << "The move was made successfully!" << endl;
+		return true;
+	}
+
+	// Displays the game board
+	void print() {
+		cout << "Hex board now:" << endl;
+		for (int i = 0; i < size - 1; i++) {
+			printStrs(2 * i + 1, " ");
+			for (int j = 0; j < size - 1; j++) cout << board[i][j] << " - ";
+			cout << board[i][size - 1] << endl;
+			printStrs(2 * i + 1, " ");
+			printStrs(size - 1, " \\ /"); cout << " \\" << endl;
+		}
+		printStrs(2 * (size - 1) + 1, " ");
+		for (int j = 0; j < size - 1; j++) cout << board[size - 1][j] << " - ";
+		cout << board[size - 1][size - 1] << endl;
+	}
+};
