8aac867fac412eba829eaae449772e886623f2bb
TheMegistone4Ever <zeusmobilenick@gmail.com>
2022-08-15 13:58:09 +0300

    Добавьте файлы проекта.

---
+++ Coursera4CPPHexProgram/Coursera4CPPHexProgram.cpp
@@ -0,0 +1,13 @@
+﻿// Peer-graded Assignment: Homework 1: Implement Hex Board,
+// by Nickita Kyselyov, NTUU KPI nb. Igor Sikorsky, 2nd grade, FICT, IS-13,
+// 15.08.2022
+
+#include "HexBoardASCII.h"
+
+int main(int argc, char const* argv[]) {
+    srand(static_cast<unsigned int>(time(nullptr)));
+
+
+
+    return EXIT_SUCCESS;
+}
