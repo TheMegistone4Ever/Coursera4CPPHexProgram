852e5f5fc05f7149e499a3974b9c9108a3054a34
TheMegistone4Ever <zeusmobilenick@gmail.com>
2022-08-17 17:25:34 +0300

    some fixes

--- Coursera4CPPHexProgram/Coursera4CPPHexProgram.cpp
+++ Coursera4CPPHexProgram/Coursera4CPPHexProgram.cpp
@@ -14,8 +14,8 @@ int main(int argc, char const* argv[]) {
     for (short k = 0; k < size * size; k++) {
         cout << "Player #" << k % 2 + 1 << " make move! (Input \'i\' and \'j\')" << endl;
         try {
-        short i;  cout << "i: "; cin >> i;
-        short j;  cout << "j: "; cin >> j;
+            short i;  cout << "i: "; cin >> i;
+            short j;  cout << "j: "; cin >> j;
             if (!board.makeMoveIn(i, j, static_cast<player>(k % 2 + 1))) k--;
         } catch (const exception& e) { break; }
         board.print();
