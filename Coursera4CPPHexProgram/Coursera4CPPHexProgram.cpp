aea3923252be5a25caea021bde9050d99f2770e7
TheMegistone4Ever <zeusmobilenick@gmail.com>
2022-08-19 22:36:15 +0300

    pre AI

--- Coursera4CPPHexProgram/Coursera4CPPHexProgram.cpp
+++ Coursera4CPPHexProgram/Coursera4CPPHexProgram.cpp
@@ -8,20 +8,38 @@ int main(int argc, char const* argv[]) {
     //srand(static_cast<unsigned int>(time(nullptr)));
     short size; cout << "Input size of a game: "; cin >> size;
     bool isAI; cout << "What game mode do you want? (0-player vs player, 1-player vs AI) "; cin >> isAI;
-    bool first; cout << "Who will go first? " << (isAI ? "(0-AI, 1-player) " : "(0-player #1, 1-player #2) "); cin >> first;
+    bool first; cout << "Who will go first? (0-" << (isAI ? "AI, 1-player) " : "player #1, 1-player #2) "); cin >> first;
 
 
     HexBoard board(size);
     board.print();
     player win;
-    for (short k = 0; (win = board.hasWon()) == player::NONE || k < size * size; k++) {
-        cout << "Player #" << k % 2 + 1 << " make move! (Input \'i\' and \'j\')" << endl;
-        short i, j;  cout << "i: "; cin >> i; cout << "j: "; cin >> j;
-        if (!board.makeMoveIn(i, j, static_cast<player>(k % 2 + 1))) k--;
-        board.print();
+
+    if (isAI) {
+        for (short k = 0; k < size * size; k++) {
+            cout << "Player #" << (k + first) % 2 + 1 << " make move! (Input \'i\' and \'j\')" << endl;
+            short i, j;
+            if ((k + first) % 2) { cout << "i: "; cin >> i; cout << "j: "; cin >> j; }
+            else {
+                pair<short, short> move = board.calcBestMove(static_cast<player>((k + first) % 2 + 1));
+                i = move.first, j = move.second;
+            }
+            if (!board.makeMoveIn(i, j, static_cast<player>((k + first) % 2 + 1))) k--;
+            board.print();
+            if ((win = board.hasWon()) != player::NONE) break;
+        }
     }
-    cout << "WON PLAYER: " << win << endl;
+    else {
+        for (short k = 0; k < size * size; k++) {
+            cout << "Player #" << (k + first) % 2 + 1 << " make move! (Input \'i\' and \'j\')" << endl;
+            short i, j;  cout << "i: "; cin >> i; cout << "j: "; cin >> j;
+            if (!board.makeMoveIn(i, j, static_cast<player>((k + first) % 2 + 1))) k--;
+            board.print();
+            if ((win = board.hasWon()) != player::NONE) break;
+        }
+    }
 
 
+    cout << "WINNER: " << win << endl;
     return EXIT_SUCCESS;
 }
